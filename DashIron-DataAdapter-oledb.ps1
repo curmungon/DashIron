dd103434d7bf6733126ecf49bb0f33e0ce1fad3b
Nathan Moyer <ne_st185@yahoo.com>
2019-09-28 00:19:48 -0700

    Updated Param defaults. Added more "Actions" Moved the insert functionality to "New-Record", update to "Update-Record" Enabled insert, update, and upsert Actions, default handler is upsert.

--- DashIron-DataAdapter-oledb.ps1
+++ DashIron-DataAdapter-oledb.ps1
@@ -18,15 +18,102 @@ param (
     [object] $params,
     [ValidateSet("select", "insert", "update", "upsert", "delete", $null)]
     [string] $Action = $(if ($params.Action) { $params.Action } else { $null }),
-    [string] $SourceTable = $(if ($params.SourceTable) { $params.SourceTable } else { '[zTestTable0]' }),
+    [string] $SourceTable = $(if ($params.SourceTable) { $params.SourceTable } else { '[TestTable]' }),
     [string] $SourceInstance = $(if ($params.Sourceinstance) { $params.Sourceinstance } else { '.\' }),
     [string] $SourceDatabase = $(if ($params.Sourcedatabase) { $params.Sourcedatabase } else { 'TestDB.accdb' }),
-    [string] $WhereFilter = $(if ($params.WhereFilter) { $params.WhereFilter } else { '1=1' }),
+    [string] $WhereFilter = $(if ($params.WhereFilter) { $params.WhereFilter } else { '' }),
     [string] $Provider = $(if ($params.Provider) { $params.Provider } else { 'Microsoft.ACE.OLEDB.12.0' }),
     [string] $ConnectionString = $(if ($params.ConnectionString) { $params.ConnectionString } else { $null }),
     $SQL = $(if ($params.SQL) { $params.SQL } else { $null }),
     $Value = $(if ($params.Value) { $params.Value } else { $null })
 )
+
+function New-Record {
+    param (
+    )
+    if ($Value.GetType().Name -notlike "*object") {
+        return ErrorMessage("The Value must be valid JSON. `n`nThere was an error processing: `n$Value")
+    }
+
+    $DataAdapter.Update($table) > $null
+
+    $newrow = $table.NewRow()
+    $Value | Get-Member -MemberType *Property | ForEach-Object {
+        if ($table.PrimaryKey.ColumnName -eq ($_.Name) ) {
+            if ($table.PrimaryKey.AutoIncrement -eq $false ) {
+                $newrow[($_.Name)] = $Value.($_.Name)
+            }
+        }
+        else {
+            # because the data is going through a DataAdapter $null needs to become DBNull
+            if ($Value.($_.Name) -eq $null) {
+                $newrow[($_.Name)] = [DBNull]::Value
+            }
+            else {
+                $newrow[($_.Name)] = $Value.($_.Name)
+            }
+        }
+    }
+    # add the new row
+    $table.Rows.Add($newrow)
+    # store only the new row
+    $dc = $table.GetChanges()
+    
+    #$newRowRollup = @{ }
+    #$newRowRollup.Add("row", @() )
+    
+    # Update the database, inserting the new rows.
+    # Capture the number of affected items
+    $effect.inserted = $DataAdapter.Update($dc)
+
+    <# 
+    # return the inserted rows as a seperate object
+    foreach ($row in $dc.Rows) {
+        $temp = @{ };
+        $columNames.foreach( { $temp.add($_, $row[$_]) } )
+        $newRowRollup.row += , $temp
+    }
+    $temp = $null;
+    $effect.add('newRows', $newRowRollup )
+    #>
+
+    # $DataAdapter.Update($dc) > $null
+    # $DataAdapter.Update($table) > $null
+
+    # these two are pointless if table = dc below
+    #$table.Merge($dc)
+    #$table.AcceptChanges()
+
+    # setting the table equal to dc ensures that we are only returning the change
+    # This will only return the changed entry regardless of what the query was
+    $table = $dc
+}
+
+function Update-Record {
+    param (
+    )
+    if ($Value.GetType().Name -notlike "*object") {
+        return ErrorMessage("The Value must be valid JSON. `n`nThere was an error processing: `n$Value")
+    }
+
+    $Value | Get-Member -MemberType *Property | ForEach-Object {
+        if ($table.PrimaryKey.ColumnName -eq ($_.Name) ) {
+            if ($table.PrimaryKey.AutoIncrement -eq $false ) {
+                $table.Rows[0][($_.Name)] = $Value.($_.Name)
+            }
+        }
+        else {
+            # because the data is going through a DataAdapter $null needs to become DBNull
+            if ($null -eq $Value.($_.Name)) {
+                $table.Rows[0][($_.Name)] = [DBNull]::Value
+            }
+            else {
+                $table.Rows[0][($_.Name)] = $Value.($_.Name)
+            }
+        }
+    }
+    $effect.updated = $DataAdapter.Update($table)
+}
 
 try {
     # check the connection string
@@ -78,7 +165,7 @@ try {
                 }
             }
             # if the SELECT is missing a WHERE clause and a valid $WhereFilter was passed seperately add it here
-            if (!$strSql.Contains("WHERE") -and $WhereFilter.Substring(0, 5) -eq "WHERE") {
+            if (!$strSql.Contains("WHERE") -and $WhereFilter.Length -gt 5 -and $WhereFilter.Substring(0, 5) -eq "WHERE") {
                 $strSql += " $WhereFilter"
             }
         }
@@ -165,12 +252,13 @@ try {
     # dataset can be filled using a single parameter once a "SelectCommand" has been set on the DataAdapter
     # the second parameter is the table's name within the dataset, access it with "dataset.Tables[<tablename>]"" 
     $DataAdapter.Fill($dataset, $SourceTable) > $Null
-
-    $table = $dataset.Tables[$SourceTable]
+    
+    [System.Data.DataTable] $table = $dataset.Tables[$SourceTable]
     $columNames = $table.Columns.ColumnName
     
     # provision our data hash table
     $data = [ordered] @{ }
+    # effect will be used to capture and send back the number of changes
     $effect = @{ }
     $effect.add('updated', 0)
     $effect.add('inserted', 0)
@@ -181,110 +269,49 @@ try {
     # when attempting to update a non-existant record, which is interpereted as an insert for now, 
     # the WHERE clause of the SQL statement keeps the new record's auto generated PK from showing up in the results
     # Obviously this needs to be resolved... upsert or some similar concept would be preferable
-    if (!$Action) {
-        if ($Value) {
-            if ($Value.GetType().Name -notlike "*object") {
-                return ErrorMessage("The Value must be valid JSON. `n`nThere was an error processing: `n$Value")
+    
+    Switch ( $Action ) {
+        "delete" {
+            # only allow single record deletes to help avoid accidental deletion
+            if ($table.Rows.Count -eq 1) {
+                $table.Rows[0].Delete()
+                $effect.deleted = $DataAdapter.Update($table)
             }
-
-            # this is essentially an INSERT... for now
-            # there are multiple records but data was submitted, put the submitted data in a new row
-            if ($table.Rows.Count -ne 1) {
-
-                $DataAdapter.Update($table) > $null
-
-                $newrow = $table.NewRow()
-                $Value | Get-Member -MemberType *Property | ForEach-Object {
-                    if ($table.PrimaryKey.ColumnName -eq ($_.Name) ) {
-                        if ($table.PrimaryKey.AutoIncrement -eq $false ) {
-                            $newrow[($_.Name)] = $Value.($_.Name)
-                        }
-                    }
-                    else {
-                        # because the data is going through a DataAdapter $null needs to become DBNull
-                        if ($Value.($_.Name) -eq $null) {
-                            $newrow[($_.Name)] = [DBNull]::Value
-                        }
-                        else {
-                            $newrow[($_.Name)] = $Value.($_.Name)
-                        }
-                    }
-                }
-                # add the new row
-                $table.Rows.Add($newrow)
-                # store only the new row
-                $dc = $table.GetChanges()
-                
-                #$newRowRollup = @{ }
-                #$newRowRollup.Add("row", @() )
-                
-                # Update the database, inserting the new rows.
-                # Capture the number of affected items
-                $effect.inserted = $DataAdapter.Update($dc)
-
-                <#
-                foreach ($row in $dc.Rows) {
-                    $temp = @{ };
-                    $columNames.foreach( { $temp.add($_, $row[$_]) } )
-                    $newRowRollup.row += , $temp
-                }
-                $temp = $null;
-                $effect.add('newRows', $newRowRollup )
-                #>
-
-                # $DataAdapter.Update($dc) > $null
-                # $DataAdapter.Update($table) > $null
-
-                # these two are pointless if table = dc below
-                $table.Merge($dc)
-                $table.AcceptChanges()
-
-                # setting the table equal to dc ensures that we are only returning the change
-                # This will only return the changed entry regardless of what the query was
-                #$table = $dc
-            }
-
-            # if there is only one row and data is recieved , execute an update
-            else {
-                $Value | Get-Member -MemberType *Property | ForEach-Object {
-                    if ($table.PrimaryKey.ColumnName -eq ($_.Name) ) {
-                        if ($table.PrimaryKey.AutoIncrement -eq $false ) {
-                            $table.Rows[0][($_.Name)] = $Value.($_.Name)
-                        }
-                    }
-                    else {
-                        # because the data is going through a DataAdapter $null needs to become DBNull
-                        if ($Value.($_.Name) -eq $null) {
-                            $table.Rows[0][($_.Name)] = [DBNull]::Value
-                        }
-                        else {
-                            $table.Rows[0][($_.Name)] = $Value.($_.Name)
-                        }
-                    }
-                }
-                $effect.updated = $DataAdapter.Update($table)
-            }
+            break
         }
-    }
-    else {
-        Switch ( $Action ) {
-            "delete" {
-                # only allow single record deletes to help avoid accidental deletion
-                if ($table.Rows.Count -eq 1) {
-                    $table.Rows[0].Delete()
-                    $effect.deleted = $DataAdapter.Update($table)
+        "insert" {
+            New-Record
+            break
+        }
+        "update" {
+            Update-Record
+            break
+        }
+        "upsert" {
+            # there isn't a specific record to work with but data was submitted, put the submitted data in a new row
+            if ($table.Rows.Count -ne 1 -or $WhereFilter -eq "") {
+                New-Record
+            }
+            # if there is only one row and data is recieved, execute an update
+            else {
+                Update-Record
+            }
+            break
+        }
+        default {
+            # if there's no Value it's likely a SELECT, don't do anything
+            # also the null value will cause errors in the other functions
+            if ($Value) {
+                # if no specific action is sent in, perform an "Upsert" operation...
+                # i.e. insert if ambiguous, update if a specific record is found
+                if ($table.Rows.Count -ne 1 -or $WhereFilter -eq "") {
+                    New-Record
                 }
-                break
+                else {
+                    Update-Record
+                }
             }
-            "insert" {
-                break
-            }
-            "update" {
-                break
-            }
-            "upsert" {
-                break
-            }
+            break
         }
     }
 
